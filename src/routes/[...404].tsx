aa533ba0fb73b3be24c52d9eec70f25ee4d5cbe0
doprz <52579214+doprz@users.noreply.github.com>
2024-03-25 14:15:33 -0500

    chore: run ESLint and Prettier

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,31 +1,25 @@
-import { A } from "@solidjs/router";
+import { A } from '@solidjs/router';
 
 export default function NotFound() {
-  return (
-    <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-        Not Found
-      </h1>
-      <p class="mt-8">
-        Visit{" "}
-        <a
-          href="https://solidjs.com"
-          target="_blank"
-          class="text-sky-600 hover:underline"
-        >
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
-      <p class="my-4">
-        <A href="/" class="text-sky-600 hover:underline">
-          Home
-        </A>
-        {" - "}
-        <A href="/about" class="text-sky-600 hover:underline">
-          About Page
-        </A>
-      </p>
-    </main>
-  );
+    return (
+        <main class='mx-auto p-4 text-center text-gray-700'>
+            <h1 class='max-6-xs my-16 text-6xl text-sky-700 font-thin uppercase'>Not Found</h1>
+            <p class='mt-8'>
+                Visit{' '}
+                <a href='https://solidjs.com' target='_blank' class='text-sky-600 hover:underline'>
+                    solidjs.com
+                </a>{' '}
+                to learn how to build Solid apps.
+            </p>
+            <p class='my-4'>
+                <A href='/' class='text-sky-600 hover:underline'>
+                    Home
+                </A>
+                {' - '}
+                <A href='/about' class='text-sky-600 hover:underline'>
+                    About Page
+                </A>
+            </p>
+        </main>
+    );
 }
